32b79dcbdaec85dd150cea96cac8103cb061d9ba
B-ashwini24 <ash24biradar@gmail.com>
2022-07-26 13:53:28 +0530

    4

--- fourth.js
+++ fourth.js
@@ -1,10 +1,36 @@
 let arr=[[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4]]
 let sum=0
-for(let i=0;i<arr.length;i++)
+let newarr=[]
+function bondrysum()
 {
-    for(let j=0;j<arr[0].length;j++)
-    {
-        sum=sum+arr[i][j]
-    }
-}
+
+  for(let i=0;i<arr.length;i++)
+  {
+        
+            for(let j=0;j<arr[0].length;j++)
+            {
+            		if (i == 0 || j == 0 || i == arr.length - 1|| j == arr.length - 1) 
+                 {
+                 	newarr.push(arr[i][j])
+                }
+                if(i==j && i!=0 && i!=arr.length-1)
+                {
+                	newarr.push(arr[i][j])
+                }
+                 if (i!=0 && j!=0 && (i + j) == arr.length - 1) {
+                  newarr.push(arr[i][j])
+                                 } 
+							
+                 
+            }
+       
+
+  }
+ for(let i=0;i<newarr.length;i++)
+ {
+ 			sum+=newarr[i]
+ }
 console.log(sum)
+}
+
+bondrysum()
